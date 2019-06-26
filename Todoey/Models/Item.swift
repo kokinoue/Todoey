d88586071f776704d3a29c5536b56a20a5ce7861
koki inoue <ko.18.5in@gmail.com>
2019-06-27 01:18:49 +0900

    add Items model

---
+++ Todoey/Models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by koki inoue on 2019/06/27.
+//  Copyright © 2019 koki inoue. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
